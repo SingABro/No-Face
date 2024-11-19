aacacb74d8021de28182b1d3bea8bb9e4a9ac029
kimkyungjae1112 <rudwo4861@naver.com>
2024-11-19 17:57:45 +0900

    feat : Highlight 기능 추가

---
+++ Source/CapstoneProject/Gimmick/StoryBook.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "StoryBook.generated.h"
+
+UCLASS()
+class CAPSTONEPROJECT_API AStoryBook : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AStoryBook();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnHighlighting(AActor* TouchedActor);
+
+	UFUNCTION()
+	void OnEndHighlighting(AActor* TouchedActor);
+
+	UFUNCTION()
+	void OnClick(AActor* TouchedActor, FKey ButtonPressed);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	TObjectPtr<class UBoxComponent> BoxComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	TObjectPtr<class UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(EditAnywhere, Category = "Mesh")
+	TObjectPtr<class UMaterialInstance> HighlightMaterial;
+};
