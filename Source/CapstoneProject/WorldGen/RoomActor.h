09c3c1c9a30967e8bcbe5a9e588454af2c34bcf3
kimkyungjae1112 <rudwo4861@naver.com>
2024-11-20 22:45:56 +0900

    feat : 맵 생성

--- Source/CapstoneProject/WorldGen/RoomActor.h
+++ Source/CapstoneProject/WorldGen/RoomActor.h
@@ -13,6 +13,18 @@ public:
     ARoomActor();
 
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room")
+    void SetRoomInfo(int32 InIdentity, const FVector& InLocation, bool InbIsEndRoom);
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Room")
     TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+    UPROPERTY(EditAnywhere, Category = "Room")
+    int32 Identity;  // 방의 정체성 (비트 플래그)
+
+    UPROPERTY(EditAnywhere, Category = "Room")
+    FVector Location;  // 방의 월드 좌표
+
+    UPROPERTY(EditAnywhere, Category = "Room")
+    bool bIsEndRoom;  // 마지막 방 여부
 };
