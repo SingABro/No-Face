f28966c29157e11d5c35349b868649412c69ae39
LarryChoi <stmcskd123@naver.com>
2024-11-23 00:57:06 +0900

    맵 됐나

--- Source/CapstoneProject/WorldGen/MyActorDuplicator.cpp
+++ Source/CapstoneProject/WorldGen/MyActorDuplicator.cpp
@@ -19,6 +19,7 @@ void AMyActorDuplicator::BeginPlay()
         FRoom StartRoom;
         StartRoom.Location = FVector3d(0, 0, 0);
         StartRoom.Identity = 0;
+        StartRoom.bIsStartRoom = true;
         // 시작 좌표
         FIntPoint StartCoords = FIntPoint(0, 0);
 
@@ -31,7 +32,7 @@ void AMyActorDuplicator::BeginPlay()
             CreateRooms(StartCoords, MaxDepth, EDirection::DOWN, EDirection::DOWN, BossRoomDirection);
             CreateRooms(StartCoords, MaxDepth, EDirection::RIGHT, EDirection::RIGHT, BossRoomDirection);
             CreateRooms(StartCoords, MaxDepth, EDirection::LEFT, EDirection::LEFT, BossRoomDirection);
-        } while (WorldMap.GetAllocatedSize() < 25);
+        } while (WorldMap.Num() > 25);
 
         BuildActualStage(WorldMap);
     }
@@ -64,11 +65,12 @@ void AMyActorDuplicator::CreateRooms(const FIntPoint& CurrentCoords, int32 Depth
     NewRoom.Identity = (static_cast<int32>(CurrentDir) * 4) % 15 + (static_cast<int32>(NewDir));
     NewRoom.bIsEndRoom = false;
     NewRoom.bIsBossRoom = false;
+    NewRoom.stretch = MaxDepth - Depth + 1; // 몇 번 뻗어나갔는지 저장
     if (WorldMap.Find(NewCoords) != NULL)
     { // 이미 월드에 존재하면
         FRoom tmpRoom;
         WorldMap.RemoveAndCopyValue(NewCoords, tmpRoom); // 기존 월드에 같은 좌표의 맵을 불러오고 삭제
-        tmpRoom.Identity |= NewRoom.Identity; // identity 갱신
+        tmpRoom.Identity = tmpRoom.Identity | NewRoom.Identity; // 두 Identity를 합침
         tmpRoom.Location = GridToWorld(NewCoords);
         tmpRoom.bIsEndRoom = false;
         tmpRoom.bIsBossRoom = false;
@@ -97,6 +99,29 @@ void AMyActorDuplicator::CreateRooms(const FIntPoint& CurrentCoords, int32 Depth
 // 방 Actor 생성
 void AMyActorDuplicator::BuildActualStage(TMap<FIntPoint, FRoom> WMap)
 {
+    bool BossRoomExist = false; // 보스 방이 있는지 확인
+
+    for (auto& Elem : WMap) // WMap의 모든 요소에 대해 반복
+    {
+        FRoom* tmpRoom = &Elem.Value; // Elem의 Value를 tmpRoom에 저장
+        if (tmpRoom->bIsBossRoom) // 보스 방이 있다면
+        {
+            BossRoomExist = true; // 보스 방이 있다고 표시
+            break; // 반복문 종료
+        }
+    }
+    if (!BossRoomExist) // 보스 방이 없다면
+    {
+        for (auto& Elem : WMap) // WMap의 모든 요소에 대해 반복
+        {
+            FRoom* tmpRoom = &Elem.Value; // Elem의 Value를 tmpRoom에 저장
+            if (tmpRoom->bIsEndRoom) // 마지막 방이라면
+            {
+                tmpRoom->bIsBossRoom = true; // 보스 방으로 설정
+                break; // 반복문 종료
+            }
+        }
+    }
     for (int i = -7; i < 8; i++)
     {
         for (int j = -7; j < 8; j++)
@@ -108,12 +133,28 @@ void AMyActorDuplicator::BuildActualStage(TMap<FIntPoint, FRoom> WMap)
                 ARoomActor* RoomActor = GetWorld()->SpawnActor<ARoomActor>(RoomActorClass, tmpRoom->Location, FRotator::ZeroRotator);
                 if (RoomActor)
                 {
-                    RoomActor->SetActorScale3D(FVector(1.0f));
-                    RoomActor->SetRoomInfo(tmpRoom->Identity, tmpRoom->Location, tmpRoom->bIsEndRoom, tmpRoom->bIsBossRoom);
+                    /*
+                    // 보스 방이거나 시작 방이라면 크기를 키움
+                    if (tmpRoom->bIsBossRoom || tmpRoom->bIsStartRoom)
+                    {
+                        RoomActor->SetActorScale3D(FVector(1.3f));
+                    }
+                    else
+                    {
+                        RoomActor->SetActorScale3D(FVector(1.f));
+                    }
+                    */ // 이제 보스방이랑 시작방 되는 거 확인했으니 주석처리
+                    RoomActor->SetActorScale3D(FVector(1.f));
+                    RoomActor->SetRoomInfo(tmpRoom->Identity, tmpRoom->Location, tmpRoom->bIsEndRoom, tmpRoom->bIsBossRoom, tmpRoom->bIsStartRoom, tmpRoom->stretch);
                 }
+
+				//RoomActor->SetActorScale3D(FVector(0.8f));
+				ARoomActor* MinimapRoomActor = GetWorld()->SpawnActor<ARoomActor>(RoomActorClass, ((tmpRoom->Location)/3000.f*1850.f)*0.5+MinimapOffset , FRotator::ZeroRotator);
+                MinimapRoomActor->SetActorScale3D(FVector(0.5f));
             }
         }
     }
+
 }
 
 EDirection AMyActorDuplicator::WhereIsBossRoom()
