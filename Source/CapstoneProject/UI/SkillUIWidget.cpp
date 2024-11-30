f6af9e964b38a710fde8af8db74b1ce3f45c0945
kimkyungjae1112 <rudwo4861@naver.com>
2024-11-30 22:52:39 +0900

    다시 되돌려놓음

---
+++ Source/CapstoneProject/UI/SkillUIWidget.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/SkillUIWidget.h"
+
+USkillUIWidget::USkillUIWidget(const FObjectInitializer& OnjectInitializer) : Super(OnjectInitializer)
+{
+
+}
+
+void USkillUIWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
